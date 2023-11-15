cb6b5acec89d328aeb494eaf74f4336b2f04f445
zqq454224016 <454224016@qq.com>
2023-11-15 13:56:08 +0800

    feat: develop model management API
    Signed-off-by: zqq454224016 <454224016@qq.com>

--- graphql-server/go-server/graph/datasource.resolvers.go
+++ graphql-server/go-server/graph/datasource.resolvers.go
@@ -12,6 +12,7 @@ import (
 	"github.com/kubeagi/arcadia/graphql-server/go-server/pkg/auth"
 	"github.com/kubeagi/arcadia/graphql-server/go-server/pkg/client"
 	"github.com/kubeagi/arcadia/graphql-server/go-server/pkg/datasource"
+	md "github.com/kubeagi/arcadia/graphql-server/go-server/pkg/model"
 )
 
 // CreateDatasource is the resolver for the createDatasource field.
@@ -81,6 +82,70 @@ func (r *mutationResolver) DeleteDatasource(ctx context.Context, input *model.De
 	return datasource.DeleteDatasource(ctx, c, name, input.Namespace, labelSelector, fieldSelector)
 }
 
+// CreateModel is the resolver for the createModel field.
+func (r *mutationResolver) CreateModel(ctx context.Context, input model.CreateModelInput) (*model.Model, error) {
+	token := auth.ForOIDCToken(ctx)
+	c, err := client.GetClient(token)
+	if err != nil {
+		return nil, err
+	}
+
+	displayname, description, filed, modeltype := "", "", "", ""
+
+	if input.DisplayName != "" {
+		displayname = input.DisplayName
+	}
+	if input.Description != nil {
+		description = *input.Description
+	}
+	if input.Field != "" {
+		filed = input.Field
+	}
+	if input.Modeltype != "" {
+		modeltype = input.Modeltype
+	}
+	return md.CreateModel(ctx, c, input.Name, input.Namespace, displayname, description, filed, modeltype)
+}
+
+// UpdateModel is the resolver for the updateModel field.
+func (r *mutationResolver) UpdateModel(ctx context.Context, input *model.UpdateModelInput) (*model.Model, error) {
+	token := auth.ForOIDCToken(ctx)
+	c, err := client.GetClient(token)
+	if err != nil {
+		return nil, err
+	}
+	name, displayname := "", ""
+	if input.DisplayName != "" {
+		displayname = input.DisplayName
+	}
+	if input.Name != "" {
+		name = input.Name
+
+	}
+	return md.UpdateModel(ctx, c, name, input.Namespace, displayname)
+}
+
+// DeleteModel is the resolver for the deleteModel field.
+func (r *mutationResolver) DeleteModel(ctx context.Context, input *model.DeleteModelInput) (*string, error) {
+	token := auth.ForOIDCToken(ctx)
+	c, err := client.GetClient(token)
+	if err != nil {
+		return nil, err
+	}
+	name := ""
+	labelSelector, fieldSelector := "", ""
+	if input.Name != nil {
+		name = *input.Name
+	}
+	if input.FieldSelector != nil {
+		fieldSelector = *input.FieldSelector
+	}
+	if input.LabelSelector != nil {
+		labelSelector = *input.LabelSelector
+	}
+	return md.DeleteModel(ctx, c, name, input.Namespace, labelSelector, fieldSelector)
+}
+
 // Datasource is the resolver for the datasource field.
 func (r *queryResolver) Datasource(ctx context.Context, name string, namespace string) (*model.Datasource, error) {
 	token := auth.ForOIDCToken(ctx)
@@ -135,6 +200,66 @@ func (r *queryResolver) DatasourcesPaged(ctx context.Context, input model.ListDa
 		end = totalCount
 	}
 	return &model.PaginatedDatasource{
+		TotalCount:  totalCount,
+		HasNextPage: end < totalCount,
+		Nodes:       filteredResult[(page-1)*pageSize : end],
+	}, nil
+}
+
+// GetModel is the resolver for the getModel field.
+func (r *queryResolver) GetModel(ctx context.Context, name string, namespace string) (*model.Model, error) {
+	token := auth.ForOIDCToken(ctx)
+	c, err := client.GetClient(token)
+	if err != nil {
+		return nil, err
+	}
+	return md.ReadModel(ctx, c, name, namespace)
+}
+
+// ListModels is the resolver for the listModels field.
+func (r *queryResolver) ListModels(ctx context.Context, input model.ListModelInput) (*model.PaginatedModel, error) {
+	token := auth.ForOIDCToken(ctx)
+	c, err := client.GetClient(token)
+	if err != nil {
+		return nil, err
+	}
+	name, displayName, labelSelector, fieldSelector := "", "", "", ""
+	page, pageSize := 1, 10
+	if input.Name != nil {
+		name = *input.Name
+	}
+	if input.DisplayName != nil {
+		displayName = *input.DisplayName
+	}
+	if input.FieldSelector != nil {
+		fieldSelector = *input.FieldSelector
+	}
+	if input.LabelSelector != nil {
+		labelSelector = *input.LabelSelector
+	}
+	if input.Page != nil && *input.Page > 0 {
+		page = *input.Page
+	}
+	if input.PageSize != nil && *input.PageSize > 0 {
+		pageSize = *input.PageSize
+	}
+	result, err := md.ListModels(ctx, c, input.Namespace, labelSelector, fieldSelector)
+	if err != nil {
+		return nil, err
+	}
+	var filteredResult []*model.Model
+	for idx, u := range result {
+		if (name == "" || strings.Contains(u.Name, name)) && (displayName == "" || strings.Contains(u.DisplayName, displayName)) {
+			filteredResult = append(filteredResult, result[idx])
+		}
+	}
+
+	totalCount := len(filteredResult)
+	end := page * pageSize
+	if end > totalCount {
+		end = totalCount
+	}
+	return &model.PaginatedModel{
 		TotalCount:  totalCount,
 		HasNextPage: end < totalCount,
 		Nodes:       filteredResult[(page-1)*pageSize : end],
